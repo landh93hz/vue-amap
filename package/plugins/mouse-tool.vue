2c26e12464f020f13e32988e79934f9c0e1bfacf
pinger <pinger.long@wintoo.com>
2021-05-20 18:14:33 +0800

    暂存

---
+++ package/plugins/mouse-tool.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+import EventMixin from '../mixins/events';
+import { amapLoader } from '../util/apiloader';
+
+export default {
+  name: 'amap-mouse-tool',
+  mixins: [EventMixin],
+  data() {
+    return {
+      target: null,
+      events: [
+        'draw'
+      ],
+    };
+  },
+  inject: ['getMap'],
+  mounted: {
+    createMouseTool(){
+      amapLoader.then(AMap => {
+        this.getMap(map => {
+          this.target = new AMap.MouseTool(map); 
+        });
+      });
+    }
+  }
+};
+</script>
