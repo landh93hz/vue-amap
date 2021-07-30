38e3c5cf4e2fa6723e40caa65015fe61798458cb
pinger <pinger.long@wintoo.com>
2021-07-30 14:24:29 +0800

    update: 椭圆编辑器更新

--- package/plugins/ellipse-editor.vue
+++ package/plugins/ellipse-editor.vue
@@ -10,26 +10,44 @@ import { amapLoader } from '../util/apiloader';
 export default {
   name: 'amap-ellipse-editor',
   mixins: [EventMixin],
+  props: {
+    createOptions: Object,
+    editOptions: Object,
+    movePoint: Object,
+    resizeXPoint: Object,
+    resizeYPoint: Object,
+  },
   data() {
     return {
       target: null,
-      isEditor: false,
       events: [
-        'move',
+        'addnode',
         'adjust',
+        'move',
+        'add',
         'end'
       ],
     };
   },
   inject: ['getMap'],
   created() {
-    
+    !this.options && (this.options = {});
+    const keys = Object.keys(this.$props);
+    for (const key of keys) {
+      if (this.$props[key] !== undefined) {
+        this.options[key] = this.$props[key];
+      }
+    }
+    amapLoader.then(AMap => {
+      const version = AMap.v || AMap.version;
+      version === '2.0' && this.createEllipseEditor();
+    });
   },
   methods: {
-    createEllipseEditor(editorObject){
+    createEllipseEditor(editorObject = undefined){
       amapLoader.then(AMap => {
         this.getMap(map => {
-          this.target = new AMap.EllipseEditor(map, editorObject);
+          this.target = new AMap.EllipseEditor(map, editorObject, this.options);
         });
       });
     }
