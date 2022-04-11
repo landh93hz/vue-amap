f8c8a2cede97fc92646a60691e7f46dff7d20b31
pinger <pinger.long@wintoo.com>
2022-04-11 14:08:00 +0800

    feat: build

--- vuepress-vueamap/deploy.sh
+++ vuepress-vueamap/deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:landh93hz/vue-amap.git master:gh-pages
+git push -f git@github.com:landh93hz/vue-amap.git ping-2.0:gh-pages
 
 cd -
